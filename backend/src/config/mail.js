4e0534c7de33bd428d067821841bb4847fccdd26
keony1 <keony@edu.univali.br>
2019-10-18 10:13:43 -0300

    Tratamento de erros, variáveis de ambiente e listando horários disponíveis

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '59dc6f5decfce0',
-    pass: '5c03ecb3c17cfc',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Keony Willian Schröer <noreply@gobarber.com.br>',
