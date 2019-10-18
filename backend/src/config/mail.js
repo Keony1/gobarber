bfc9562e66e84a0f64c87f6a4c1d07cae2dc7f57
Keony W. Schröer <keony@edu.univali.br>
2019-10-18 00:03:43 -0300

    Adicionado filas, novas rotas, e envio de email de notificação

---
+++ backend/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '59dc6f5decfce0',
+    pass: '5c03ecb3c17cfc',
+  },
+  default: {
+    from: 'Keony Willian Schröer <noreply@gobarber.com.br>',
+  },
+};
